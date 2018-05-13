0b5e622dbb93f630c37b21cc7ac8b61e8a2d25b5
Pierre <dev@net-tools.ovh>
2018-05-13 14:08:08 +0200

    hash_equals bugfix

--- src/SecurityHandlers/HashSecurityHandler.php
+++ src/SecurityHandlers/HashSecurityHandler.php
@@ -62,15 +62,11 @@ class HashSecurityHandler extends SecurityHandler {
 	 */
 	public function check(\Nettools\Simple_Framework\Request $req)
 	{
-		try
-		{
-			return hash_equals(self::makeHash($req->{$this->_idparam}, $this->_secret), $req->{$this->_hparam});
-		}
-		// catching when one of hash_equals parameters is null (E_WARNING)
-		catch(\ErrorException $e)
-		{
-			return false;
-		}
+		$t = $req->{$this->_hparam};
+		if ( is_null($t) )
+			$t = '';
+		
+		return hash_equals(self::makeHash($req->{$this->_idparam}, $this->_secret), $t);
 	}
 	
 }
