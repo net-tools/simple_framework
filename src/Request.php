01bdfc867d54ca51ba931bff47b0770caed256d5
Pierre <dev@nettools.ovh>
2022-03-03 12:16:57 +0100

    php8

--- src/Request.php
+++ src/Request.php
@@ -64,7 +64,7 @@ class Request {
         if ( !is_string($k) )
             throw new Exceptions\InvalidParameterException("'k' parameter is not a string.");
         
-        return isset($this->_params[$k]);
+        return !empty($this->_params[$k]);
     }
     
     
@@ -100,7 +100,7 @@ class Request {
         if ( !is_string($k) )
             throw new Exceptions\InvalidParameterException("'k' parameter is not a string.");
 
-        return isset($this->_fileUploads[$k]);
+        return !empty($this->_fileUploads[$k]);
 	}
     
     
